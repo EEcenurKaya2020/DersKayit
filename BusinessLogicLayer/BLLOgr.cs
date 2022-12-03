e90e0a73e667f0d1ec10889b64be814446e83e0f
Ecenur Kaya <kaya.ecenur02@gmail.com>
2022-12-03 15:28:39 +0300

    Add project files.

---
+++ BusinessLogicLayer/BLLOgr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EntityLayer;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class BLLOgr
+    {
+        public static int OgrEkleBLL(EntityOgr p)
+        {
+            if(p.Ad != null && p.Soyad != null && p.No != null && p.Sifre != null && p.Foto != null)
+            {
+                return DALOgr.OgrEkle(p);
+            }
+            return -1;
+        } 
+    }
+}
